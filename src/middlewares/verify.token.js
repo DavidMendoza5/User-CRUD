a058a43bf0171bd16d699993a059551efacff8c3
David <cruz-azul99@hotmail.com>
2022-08-10 17:09:29 -0500

    Verify token function created

---
+++ src/middlewares/verify.token.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const verifyToken = (req, res, next) => {
+  let code = 400;  
+  try {
+    const token = req.headers.authorization;
+      if (!token) {
+        code = 401;
+        throw new Error('Acceso denegado');
+      }
+
+      const verified = jwt.verify(token.split(' ')[1], process.env.JWT_SECRET);
+      req.user = verified
+      next()
+    } catch (error) {
+      res.status(code).json({error: 'El token no es válido'})
+    }
+}
+
+module.exports = verifyToken;
