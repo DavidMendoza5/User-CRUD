4936cb49962305ad25e8a7482fb950ba11685b97
David <cruz-azul99@hotmail.com>
2022-08-10 16:02:40 -0500

    Id validation function created

---
+++ src/middlewares/validate.id.js
@@ -0,0 +1,15 @@
+const repository = require('../repository/abstract.repository');
+
+module.exports = (schema, reqProperty) => async (req, res, next) => {
+  try {
+    const id = { id: req[reqProperty].id };
+    const exist = await repository.getById(schema, id);
+    if (!exist) {
+      throw new Error(`No se encontró el dato con ID: ${req[reqProperty].id}`);
+    };
+
+    next();
+  } catch (err) {
+    res.status(404).send({ message: err.message });
+  }
+};
