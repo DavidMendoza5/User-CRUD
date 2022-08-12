619b3f1543b9a27e0db85600e9b86fd3a5f06b1c
David <cruz-azul99@hotmail.com>
2022-08-12 09:54:02 -0500

    Email validation added

--- src/middlewares/user.validation.js
+++ src/middlewares/user.validation.js
@@ -11,6 +11,12 @@ const create = Joi.object({
     'string.empty': 'El username no debe ser un texto vacío',
     'any.required': 'El username es un campo requerido',
   }),
+  email: Joi.string().trim().email().required().messages({
+    'string.base': 'El correo electrónico debe ser un texto',
+    'string.empty': 'El correo electrónico no debe ser un texto vacío',
+    'string.email': 'La correo electrónico tiene un formato inválido',
+    'any.required': 'El correo electrónico es un campo requerido',
+  }),
   password: Joi.string().trim().required().messages({
     'string.base': 'La contraseña debe ser un texto',
     'string.empty': 'La contraseña no debe ser un texto vacío',
