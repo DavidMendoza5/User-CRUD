b106117f97d8decd6ec85230237ba1631e116f46
David <cruz-azul99@hotmail.com>
2022-08-16 07:31:19 -0500

    Tests modified

--- test/agent/agent.test.js
+++ test/agent/agent.test.js
@@ -3,23 +3,6 @@ const app = require('../../src/app');
 const userService = require('../../src/services/user.service');
 
 describe('API /agents', () => {
-    test('Login', async () => {
-      // const response = await userService.logInUserService({
-      //   "email": "user@gmail.com",
-      //   "password": "12345"
-      // });
-      const response = await request(app).post('/api/login')
-        .send({
-          "email": "user@gmail.com",
-          "password": "12345"
-        });
-      //   console.log(response);
-
-      console.log(response);
-      // expect(response.code).toEqual(200);
-      expect(response.statusCode).toBe(200);
-    });
-
     test('It should not login because the password is wrong', async () => {
       const response = await request(app).post('/api/login')
         .send({
--- test/insurancePolicy/insurance.policy.test.js
+++ test/insurancePolicy/insurance.policy.test.js
@@ -9,20 +9,4 @@ describe('API /insurance-policies', () => {
         
         expect(response.statusCode).toBe(200);
     });
-
-    // test('Should create an insurance policy', async () => {
-    //   const response = await request(app).post('/api/insurance-policies')
-    //     .set('Authorization', `Bearer ${TOKEN}`)
-    //     .send({
-    //       "startDate": "2022-06-30",
-    //       "endingDate": "2022-08-23",
-    //       "insuranceCarrier": "Aseguradora",
-    //       "policyType": "GASTOS_MEDICOS_MAYORES",
-    //       "price": 9999999,
-    //       "status": "VIGENTE",
-    //       "clientId": "22a001a4-ec0f-4323-9c0a-e32e42333b9b"
-    //     });
-
-    //   expect(response.statusCode).toBe(201);
-    // });
-})
+});
