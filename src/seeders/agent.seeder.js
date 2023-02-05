3bfd157410bac56e57996d928854d4062da5bbc9
David <cruz-azul99@hotmail.com>
2023-02-05 10:59:44 -0600

    Hardcoded in agent's credentials removed

--- src/seeders/agent.seeder.js
+++ src/seeders/agent.seeder.js
@@ -1,17 +1,18 @@
 const agentService = require('../services/user.service');
+require('dotenv').config();
 
 async function createAgentSeed() {
   try {
     const data = {
-      "name": "Admin",
-      "email": "admin@gmail.com",
-      "password": "12345"
+      "name": process.env.NAME,
+      "email": process.env.EMAIL,
+      "password": process.env.PASSWORD
     } 
     const userFound = await agentService.getUserByEmailService({email: data.email});
 
     if(Object.keys(userFound).length === 0) await agentService.createUserService(data);
   } catch (error) {
-    console.log(error);
+    throw new Error(error.message);
   }
 }
 
